36618ee1e9aef87961ad5665f1eadd12516a98c2
Heather Cartwright <heather.cartwright@live.co.uk>
2021-08-17 15:48:37 +0100

    Linked together front end with API helpers

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # create an application and run the application
 from website import create_app
+from flask_session import Session
 
 app = create_app()
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # only run if you execute this file
 if __name__ == '__main__': 
